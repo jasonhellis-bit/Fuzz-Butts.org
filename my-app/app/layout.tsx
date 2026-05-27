e831506a827f3d28d635ed4cbc3116a3ec911929
jasonhellis-bit <jason.h.ellis@gmail.com>
2026-05-26 21:47:24 -0700

    Change the favicon to cat paws icon.

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -24,6 +24,9 @@ export const metadata: Metadata = {
   title: "Fuzz Butts Cat Rescue",
   description:
     "A nonprofit dedicated to rescuing cats and helping them find safe, loving homes.",
+  icons: {
+    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🐾</text></svg>",
+  },
 };
 
 export default function RootLayout({
